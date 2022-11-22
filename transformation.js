1c105f5d013c800e19bf50c4ce4db738fc9232a5
Your Name <you@example.com>
2022-11-22 09:49:18 -0500

    fixing

--- transformation.js
+++ transformation.js
@@ -3,7 +3,11 @@
  * and when you call restore, the state on top of the stack is
  * taken off and used as the context’s current transformation
  */
-let cx = document.querySelector("canvas").getContext("2d");
+let canvas = document.querySelector("canvas");
+let cx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
 function branch(length, angle, scale) {
   cx.fillRect(0, 0, 1, length);
   if (length < 8) return;
@@ -13,7 +17,7 @@ function branch(length, angle, scale) {
   branch(length * scale, angle, scale);
   cx.rotate(2 * angle);
   branch(length * scale, angle, scale);
-  //cx.rotate();
+  cx.restore();
 }
-cx.translate(300, 0);
+cx.translate(300, 0); //move tree 300 px along the x axis
 branch(60, 0.5, 0.8);
